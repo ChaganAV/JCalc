b30762c99ac6c0506b6b890d35a8c44dc225c50b
Alexandr <jwchaganav@gmail.com>
2023-10-16 14:41:37 +0400

    calc

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -86,11 +86,4 @@ public class Calculation{
         }
         return true;
     }
-    public static <T extends Comparable<T>, V extends T> boolean isIn(T x, V[] y){
-        var temp = x;
-        for (int i = 0; i < y.length; i++) {
-            if(x.equals(y[i])) return true;
-        }
-        return false;
-    }
 }
