43518ea470468d54577d7b9b9dff364bb97c7353
Alexandr <jwchaganav@gmail.com>
2023-10-17 14:21:11 +0400

    исправил методы на value1.doubleValue()

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -2,61 +2,18 @@ import java.util.ArrayList;
 
 public class Calculation{
     public static <T extends Number> void sum(T value1, T value2){
-        double val1 = 0;
-        double val2 = 0;
-
-        if(value1 instanceof Integer){
-            val1 = (Integer) value1;
-        }
-        if(value1 instanceof Double){
-            val1 = (Double) value1;
-        }
-        if(value1 instanceof Float){
-            val1 = (Float) value1;
-        }
-        if(value2 instanceof Integer){
-            val2 = (Integer) value2;
-        }
-        if(value2 instanceof Double){
-            val2 = (Double) value2;
-        }
-        if(value2 instanceof Float){
-            val2 = (Float) value2;
-        }
+        double val1 = value1.doubleValue();
+        double val2 = value2.doubleValue();
         System.out.println(val1 + val2);
     }
     public static <T extends Number> void multiply(T value1, T value2){
-        double val1 = 0;
-        double val2 = 0;
-        if(value1 instanceof Integer){
-            val1 = (int) value1;
-        }
-        if(value1 instanceof Double){
-            val1 = (double) value1;
-        }
-        if(value2 instanceof Integer){
-            val2 = (int) value2;
-        }
-        if(value2 instanceof Double){
-            val2 = (double) value2;
-        }
+        double val1 = value1.doubleValue();
+        double val2 = value2.doubleValue();
         System.out.println(val1 * val2);
     }
     public static <T extends Number> void divide(T value1, T value2){
-        double val1 = 0;
-        double val2 = 0;
-        if(value1 instanceof Integer){
-            val1 = (int) value1;
-        }
-        if(value1 instanceof Double){
-            val1 = (double) value1;
-        }
-        if(value2 instanceof Integer){
-            val2 = (int) value2;
-        }
-        if(value2 instanceof Double){
-            val2 = (double) value2;
-        }
+        double val1 = value1.doubleValue();
+        double val2 = value2.doubleValue();
         if(val2 != 0)
             System.out.println(val1 / val2);
         else{
@@ -64,20 +21,8 @@ public class Calculation{
         }
     }
     public static <T extends Number> void subtract(T value1, T value2){
-        double val1 = 0;
-        double val2 = 0;
-        if(value1 instanceof Integer){
-            val1 = (int) value1;
-        }
-        if(value1 instanceof Double){
-            val1 = (double) value1;
-        }
-        if(value2 instanceof Integer){
-            val2 = (int) value2;
-        }
-        if(value2 instanceof Double){
-            val2 = (double) value2;
-        }
+        double val1 = value1.doubleValue();
+        double val2 = value2.doubleValue();
         System.out.println(val1 - val2);
     }
     public <T extends Comparable<T>,V extends T> boolean compareArrays(V[] array1, V[] array2){
