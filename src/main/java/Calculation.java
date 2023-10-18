c9c51e07286b690049854a8c2b45f235a90c59c5
Alexandr <jwchaganav@gmail.com>
2023-10-18 08:14:05 +0400

    исправил методы

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -1,29 +1,30 @@
 import java.util.ArrayList;
 
 public class Calculation{
-    public static <T extends Number> void sum(T value1, T value2){
+    public static <T extends Number> double sum(T value1, T value2){
         double val1 = value1.doubleValue();
         double val2 = value2.doubleValue();
-        System.out.println(val1 + val2);
+        return val1+val2;
     }
-    public static <T extends Number> void multiply(T value1, T value2){
+    public static <T extends Number> double multiply(T value1, T value2){
         double val1 = value1.doubleValue();
         double val2 = value2.doubleValue();
-        System.out.println(val1 * val2);
+        return val1 * val2;
     }
-    public static <T extends Number> void divide(T value1, T value2){
+    public static <T extends Number> double divide(T value1, T value2){
         double val1 = value1.doubleValue();
         double val2 = value2.doubleValue();
         if(val2 != 0)
-            System.out.println(val1 / val2);
+            return val1 / val2;
         else{
             System.out.println("Делить на 0 нельзя!");
+            return 0;
         }
     }
-    public static <T extends Number> void subtract(T value1, T value2){
+    public static <T extends Number> double subtract(T value1, T value2){
         double val1 = value1.doubleValue();
         double val2 = value2.doubleValue();
-        System.out.println(val1 - val2);
+        return val1 - val2;
     }
     public <T extends Comparable<T>,V extends T> boolean compareArrays(V[] array1, V[] array2){
         if(array1.length != array2.length) return false;
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -6,10 +6,10 @@ public class Program {
         //Calculation calc = new Calculation();
         int value1 = 3;
         double value2 = 3.4;
-        Calculation.sum(value1,value2);
-        Calculation.multiply(value1,value2);
-        Calculation.divide(value1,value2);
-        Calculation.subtract(value1,value2);
+        System.out.println(Calculation.sum(value1,value2));
+        System.out.println(Calculation.multiply(value1,value2));
+        System.out.println(Calculation.divide(value1,value2));
+        System.out.println(Calculation.subtract(value1,value2));
         // compareArray
         Integer[] numbs = new Integer[] {1,3,4,5};
         Integer[] numbs2 = new Integer[] {2,3,4,5};
